2c3b1bd00ea7ba7cd1d1105e308b1b3e9845f0c8
TheRabani <itayrabani@gmail.com>
2022-10-27 08:49:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myfinalproject/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.myfinalproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    ShopFragment shopFragment = new ShopFragment();
+    CalendarFragment calendarFragment = new CalendarFragment();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                        return true;
+                    case R.id.shop:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, shopFragment).commit();
+                        return true;
+                    case R.id.calendar:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, calendarFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
