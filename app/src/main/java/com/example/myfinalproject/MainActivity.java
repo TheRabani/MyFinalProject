aec788c3d3e2adecbb17e212a1af5fbce48ab46b
TheRabani <itayrabani@gmail.com>
2023-02-12 18:13:15 +0200

    changed bottom menu

--- app/src/main/java/com/example/myfinalproject/MainActivity.java
+++ app/src/main/java/com/example/myfinalproject/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.myfinalproject;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     HomeFragment homeFragment = new HomeFragment();
     ShopFragment shopFragment = new ShopFragment();
     CalendarFragment calendarFragment = new CalendarFragment();
+    private ChipNavigationBar chipNavigationBar;
+    private Fragment fragment = null;
 
 
     @Override
@@ -23,28 +27,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
+//        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        chipNavigationBar  = findViewById(R.id.bottom_navigation);
+//        chipNavigationBar.setBackground(R.drawable.show_gun_background);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
-        bottomNavigationView.setSelectedItemId(R.id.home);
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+//        chipNavigationBar.setBackgroundColor(Color.WHITE);
+
+        chipNavigationBar.setItemSelected(R.id.home, true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+
+//        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+//        bottomNavigationView.setSelectedItemId(R.id.home);
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(MenuItem item) {
-                switch (item.getItemId())
-                {
+            public void onItemSelected(int i) {
+                switch (i) {
                     case R.id.home:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
-                        return true;
+                        fragment = new HomeFragment();
+                        break;
                     case R.id.shop:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, shopFragment).commit();
-                        return true;
+                        fragment = new ShopFragment();
+                        break;
                     case R.id.calendar:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, calendarFragment).commit();
-                        return true;
+                        fragment = new CalendarFragment();
+                        break;
+
                 }
-                return false;
+
+                if (fragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+                }
             }
         });
+
+//        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(MenuItem item) {
+//                switch (item.getItemId())
+//                {
+//                    case R.id.home:
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+//                        return true;
+//                    case R.id.shop:
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.container, shopFragment).commit();
+//                        return true;
+//                    case R.id.calendar:
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.container, calendarFragment).commit();
+//                        return true;
+//                }
+//                return false;
+//            }
+//        });
 
     }
 }
